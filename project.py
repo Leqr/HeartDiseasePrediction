430c2bce790b53dff936a008999e6dde9a332101
Leqr <leonardeqr@icloud.com>
2020-04-29 14:49:54 +0200

    last data

--- project.py
+++ project.py
@@ -1,9 +1,11 @@
+import pandas as pd
+
 class ResultValues():
 
     def __init__(self):
-        
+
         # Do computations here
-        
+
         # Task 1
         self.arbre = None
         # Task 3
@@ -14,3 +16,12 @@ class ResultValues():
 
     def get_results(self):
         return [self.arbre, self.faits_initiaux, self.regles, self.arbre_advance]
+
+    def importData(self,filename):
+
+        """ Import data from a .csv file and transforms it into usable data containers.
+
+            :param file: .csv data file
+            :return: the data ID3 ready
+        """
+        data = pd.read_csv(filename)
